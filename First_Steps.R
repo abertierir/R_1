2aec4766bbd92975d55a5ee047ebbba6ad635530
Andrea Bertieri <andreabertieri@MacBook-Air-de-Andrea.local>
2022-09-12 21:35:53 -0500

    Vectores_first Lesson

--- First_Steps.R
+++ First_Steps.R
@@ -5,3 +5,12 @@ str(orangeec)
 summary(orangeec)
 
 summary(mtcars)
+
+?orangeec
+
+summary(mtcars)
+
+tiempo <- c(25,5,10,15,10)
+tiempo_lecturas <- c(30,10,5,10,15)
+tiempo_aprendizaje <- tiempo+tiempo_lecturas
+tiempo_aprendizaje
