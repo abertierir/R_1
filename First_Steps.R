3fc43ecb73f9300ee66b1ca06a1b72e12810e7c9
Andrea Bertieri <andreabertieri@MacBook-Air-de-Andrea.local>
2022-09-12 21:41:29 -0500

    Other type of vector

--- First_Steps.R
+++ First_Steps.R
@@ -14,3 +14,17 @@ tiempo <- c(25,5,10,15,10)
 tiempo_lecturas <- c(30,10,5,10,15)
 tiempo_aprendizaje <- tiempo+tiempo_lecturas
 tiempo_aprendizaje
+
+dias_de_aprendizaje <- c("Lunes","Martes","Miércoles", "Jueves", "Viernes")
+dias_de_aprendizaje
+
+dias_mas_20min <- c(TRUE,FALSE,FALSE,TRUE,TRUE)
+dias_mas_20min
+
+total_tiempo <-sum(tiempo)
+total_tiempo
+
+total_tiempo_lecturas <- sum(tiempo_lecturas)
+total_tiempo_lecturas
+
+total_tiempo+total_tiempo_lecturas
