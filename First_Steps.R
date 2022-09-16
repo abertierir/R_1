505f6c6bba0fcebff962486afef892c52afe8e5f
Andrea Bertieri <andreabertieri@MacBook-Air-de-Andrea.local>
2022-09-15 20:49:57 -0500

    Time Matrix First Analysis Colums sum

--- First_Steps.R
+++ First_Steps.R
@@ -15,26 +15,4 @@ tiempo_lecturas <- c(30,10,5,10,15)
 tiempo_aprendizaje <- tiempo+tiempo_lecturas
 tiempo_aprendizaje
 
-dias_de_aprendizaje <- c("Lunes","Martes","Miércoles", "Jueves", "Viernes")
-dias_de_aprendizaje
 
-dias_mas_20min <- c(TRUE,FALSE,FALSE,TRUE,TRUE)
-dias_mas_20min
-
-total_tiempo <-sum(tiempo)
-total_tiempo
-
-total_tiempo_lecturas <- sum(tiempo_lecturas)
-total_tiempo_lecturas
-
-total_tiempo+total_tiempo_lecturas
-
-
-dias_de_semana<-c("Lunes","Martes", "Miércoles", "Jueves", "Viernes")
-temas_curiosidad_martes<-c("Código detrás del main de java y python")
-tareas_panning_martes<-c("Analizar la historia y hacer un Feature Modelo", "Seguir los pasos del planning para definir criterios de aceptación y casos de prueba")
-
-x<-"Este solo es un saludito"
-x
-y<-c("Este","es","un","saludito")
-y
---
+++ Matrices_tiempo.R
@@ -0,0 +1,16 @@
+tiempo_estudio <- c(2,1,2,2,2)
+tiempo_sueño <- c(8,7.5,9,6,7.5)
+tiempo_trabajo <- c(8,8,9,8,8)
+tiempo_lectura <- c(0.5,1,2,0.5,0.2)
+
+#Matriz
+tiempo_matriz <- matrix(c(tiempo_estudio,tiempo_lectura), nrow=2, byrow=TRUE)
+dias <- c("Lunes","Martes","Miercoles", "Jueves", "Viernes")
+Tiempo <- c("Tiempo Estudio", "Tiempo Lectura")
+
+colnames(tiempo_matriz) <-dias
+rownames(tiempo_matriz) <-Tiempo
+
+tiempo_matriz
+
+colSums(tiempo_matriz)
